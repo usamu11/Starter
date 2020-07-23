eaabc1c4e1fec915497ce7ecc6c1b0e294fba352
Samuca-sys <samuel.mferreira@hotmail.com>
2020-07-23 20:12:01 -0300

    chore: add elements references

--- todos.js
+++ todos.js
@@ -1,1 +1,9 @@
-alert("Funcionou");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var toDos = [ 
+    'Fazer café',
+    'Estudar Javascipt',
+    'Acessar comunidade da Rocketseat'
+];
