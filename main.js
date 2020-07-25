f7b6feb5a7af13d9106908b01631d8dcdfc5f231
Samuca-sys <samuel.mferreira@hotmail.com>
2020-07-24 21:43:07 -0300

    chore: add first exercise list 3

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-axios.get('https://api.github.com/users/usamu11')
-    .then(function(response) {
-        console.log(response.data.avatar_url);
+/*1º exercício: Crie uma função que recebe a idade de 
+um usuário e retorna uma Promise que depois de 2 
+segundos retornará se usuário é maior ou não que 18 
+anos. Se o usuário ter mais que 18 anos deidade o 
+resultado deve cair no .then, caso contrário, no .catch
+*/
+
+function checaIdade(idade) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function () { 
+            if(idade >= 18) {
+                    resolve();
+            } else {
+                reject();
+            }
+        }, 2000)
     })
-    .catch(function(error) {
-        console.warn(error);
+}
+
+checaIdade(20)
+    .then(function(response) {
+        console.log("Maior que 18");
+    })
+    .catch(function(response) {
+        console.log("Menor que 18");
     });
